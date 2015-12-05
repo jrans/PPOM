c575a7539e582fcff7e6bc5c0443f39e571d781a
besartshyti <besartshyti@gmail.com>
2015-12-05 23:27:40 +0000

    add 'GET /song/{id} endpoint which returns mp3 stream

---
+++ test/api/handlers/stream.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const path = '../../..';
+
+const test = require('tape');
+const serverFunction = require(path+'/api/server.js');
+
+test('api:handler:stream -> ', t => {
+
+  t.end();
+});
