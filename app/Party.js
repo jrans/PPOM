3e32adc932833ddcc975f4d32df126bef874b7fa
Jack Rans <jwrans@gmail.com>
2015-12-06 00:13:53 +0000

    sdfsd

--- app/Party.js
+++ app/Party.js
@@ -1,24 +1,113 @@
 // npm
-import React, { Component, PropTypes, View, StyleSheet } from 'react-native';
-// Pages
+import React, { Component, PropTypes, View, StyleSheet, TextInput, ScrollView } from 'react-native';
+// Components
+import TrackResult from './TrackResult.js';
 
-class Home extends Component {
+class Search extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      duration: undefined,
+      time: undefined,
+      playing: ''
+    }
+
+    this.setDuration = this.setDuration.bind(this);
+    this.setTime     = this.setTime.bind(this);
+    this.play = () => {};
+  }
+
+  setDuration (duration) {
+    this.setState({duration})
+  }
+
+  setTime (time) {
+    this.setState({time})
+  }
 
   render () {
-    return <View style = {styles.container} />
+    const {
+      props: {changeState, state},
+      setTime,
+      setDuration,
+      play
+    } = this;
+
+    return (
+      <View style = {styles.container}>
+        <View style= {styles.searchBar}>
+          <TextInput
+            style         = { styles.searchInput }
+            placeholder   = "search by track, artist, keyword"
+            onChangeText  = { (value) => changeState({search:value}) }
+            value         = { state.search }
+          />
+        </View>
+        <ScrollView
+          ref                              = {component=>this._funkyScrollView=component}
+          automaticallyAdjustContentInsets = {false}
+          scrollsToTop                     = {false}
+          scrollEnabled                    = {true}
+          style                            = {styles.trackScroll}
+          scrollEventThrottle              = {16}
+          contentContainerStyle            = {styles.scrollContent}
+        >
+          <TrackResult
+            artist  = "horse"
+            title   = "my lovely horse"
+            url     = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4"
+            picture = "http://www.howrse.com/media/equideo/image/chevaux/adulte/americain/normal/300/bai_v1828806360.png"
+            play    = {play}
+            setTime = {setTime}
+            onEnd   = {setDuration}
+          />
+        </ScrollView>
+        <View style={styles.bottomBar}>
+        </View>
+      </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: 'red'
-  }
-})
+// onScroll  =   { (event) => this.setState({scrollPosition:event.nativeEvent.contentOffset.y})}
 
-Home.propTypes = {
+Search.propTypes = {
   changePage:  PropTypes.func,
-  changeState: PropTypes.func
+  changeState: PropTypes.func,
+  state:       PropTypes.object
 };
 
-export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection:   'column',
+    justifyContent:  'space-between',
+  },
+    searchBar: {
+      height: 30,
+      backgroundColor: 'blue',
+      flexDirection: 'row',
+      paddingHorizontal: 20
+    },
+      searchInput: {
+        flex: 1,
+        backgroundColor: 'yellow',
+        paddingHorizontal: 10
+      },
+    trackScroll: {
+      height: 100,
+      backgroundColor: 'orange'
+    },
+      scrollContent: {
+        flexDirection: 'column',
+        alignItems:    'stretch'
+      },
+    bottomBar: {
+      height:60
+    }
+})
+
+export default Search;
--- app/TrackResult.js
+++ app/TrackResult.js
@@ -1,37 +1,76 @@
 // npm
-import React, { Component, PropTypes, View, StyleSheet, Text } from 'react-native';
+import React, { Component, PropTypes, View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 // Pages
+import Video from 'react-native-video';
+
 
 class TrackResults extends Component {
 
   render () {
+    const {
+      artist,
+      title,
+      url,
+      picture,
+      play,
+      setTime,
+      setDuration,
+      onEnd
+    } = this.props;
+
     return (
       <View style = {styles.container} >
-        <Text style = {styles.name} >
-          NAME
+        <Video
+          resizeMode = "cover"
+          source      ={{uri: url}}
+          style       = {styles.video}
+          repeat      = {true}
+          volume      = {1.0}
+          onProgress  = {setTime}
+          onEnd       = {onEnd}
+          onLoad      = {setDuration}
+        />
+        <TouchableOpacity style = {styles.play} onPress={play}/>
+        <Image src = {picture} style = {styles.icon} />
+        <Text style = {styles.artist} >
+          {artist}
+        </Text>
+        <Text style = {styles.title} >
+          {title}
         </Text>
       </View>
     );
   }
 }
 
-TrackResults.propTypes = {
-  artist: PropTypes.string,
-  title:  PropTypes.string,
-  url:    PropTypes.string,
-};
 
 const styles = StyleSheet.create({
   container: {
     height:30,
     backgroundColor: 'grey',
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'center'
   },
-    name: {
+    video: {
+      height:0,
+      width: 0
+    },
+    artist: {
       fontSize: 20,
       color: 'blue'
+    },
+    title: {
+      fontSize: 20,
+      color: 'blue'
+    },
+    icon: {
+      height: 20,
+      width:  20,
+    },
+    play: {
+      height: 20,
+      width: 20
     }
 })
 
